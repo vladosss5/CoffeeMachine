bbdcb6996ea00a19aade0eef8201b7cde3ed748d
TOMSKASU\KiporovVA <KiporovVA@tomskasu.ru>
2024-05-02 17:26:07 +0700

    Init tests

---
+++ tests/CoffeeMachine.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CoffeeMachine.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ tests/CoffeeMachine.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CoffeeMachine.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
