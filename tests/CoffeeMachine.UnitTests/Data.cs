234c9490f802d75f42443ce06ecbb8c63811593f
TOMSKASU\KiporovVA <KiporovVA@tomskasu.ru>
2024-05-03 18:29:32 +0700

    Save 03.05

---
+++ tests/CoffeeMachine.UnitTests/Data.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.Common;
+using CoffeeMachine.Core.Models;
+using CoffeeMachine.Persistence.Data.Context;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeMachine.UnitTests;
+
+public class Data : IDisposable
+{
+    private readonly DbConnection _connection;
+    private readonly DbContextOptions<DataContext> _contextOptions;
+    private readonly DataContext _context;
+
+    public Data()
+    {
+        _connection = new SqliteConnection("Filename=:memory:");
+        _connection.Open();    
+        
+        _contextOptions = new DbContextOptionsBuilder<DataContext>()
+            .UseSqlite(_connection)
+            .Options;
+        
+        using var context = new DataContext(_contextOptions);
+
+        if (context.Database.EnsureCreated())
+        {
+            using var viewCommand = context.Database.GetDbConnection().CreateCommand();
+            viewCommand.CommandText = @"CREATE VIEW AllResources AS SELECT Url FROM CoffeeMachine;";
+            viewCommand.ExecuteNonQuery();
+        }
+        
+        context.AddRangeAsync(
+            new Coffee{Id = 1, Name = "Cappuccino", Price = 500},
+            new Coffee{Id = 2, Name = "Latte", Price = 400},
+            new Coffee{Id = 3, Name = "Espresso", Price = 300},
+            new Banknote{Id = 4, Nominal = 500},
+            new Banknote{Id = 2, Nominal = 100},
+            new Banknote{Id = 3, Nominal = 50},
+            new Machine{Id = 3, SerialNumber = "11", Description = "wdw"},
+            new BanknoteToMachine
+            {
+                Id = 1,
+                Banknote = new Banknote{Id = 4},
+                Machine = new Machine{Id = 1, SerialNumber = "11", Description = "wdw"}
+            }
+        );
+        
+        context.SaveChangesAsync();
+    }
+    public DataContext CreateContext() => new DataContext(_contextOptions);
+    
+    public void Dispose() => _connection.Dispose();
+}
--- tests/CoffeeMachine.UnitTests/Repository/BanknoteRepositoryTests.cs
+++ tests/CoffeeMachine.UnitTests/Repository/BanknoteRepositoryTests.cs
@@ -7,84 +7,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeMachine.UnitTests.Repository;
 
-[TestFixture]
-public class BanknoteRepositoryTests : IDisposable
+public class BanknoteRepositoryTests 
 {
-    private readonly DbConnection _connection;
-    private readonly DbContextOptions<DataContext> _contextOptions;
-    
-    #region ConstructorAndDispose
-    public BanknoteRepositoryTests()
-    {
-        _connection = new SqliteConnection("Filename=:memory:");
-        _connection.Open();    
-        
-        _contextOptions = new DbContextOptionsBuilder<DataContext>()
-            .UseSqlite(_connection)
-            .Options;
-        
-        using var context = new DataContext(_contextOptions);
-
-        if (context.Database.EnsureCreated())
-        {
-            using var viewCommand = context.Database.GetDbConnection().CreateCommand();
-            viewCommand.CommandText = @"CREATE VIEW AllResources AS SELECT Url FROM CoffeeMachine;";
-            viewCommand.ExecuteNonQuery();
-        }
-        
-        context.AddRange(
-            new Coffee{Id = 1, Name = "Cappuccino", Price = 500},
-            new Coffee{Id = 2, Name = "Latte", Price = 400},
-            new Coffee{Id = 3, Name = "Espresso", Price = 300}
-            );
-        context.SaveChanges();
-    }
-    
-    DataContext CreateContext() => new DataContext(_contextOptions);
-    
-    public void Dispose() => _connection.Dispose();
-    #endregion
-
     [Test]
-    public async Task TestGetAll()
+    public async Task TestGetByNominalAsync()
     {
-        using var context = CreateContext();
-        var repository = new CoffeeRepository(context);
+        var data = new Data();
+        using var context = data.CreateContext();
+        var repository = new BanknoteRepository(context);
         
-        var coffees = await repository.GetAllAsync();
+        var banknote = await repository.GetByNominalAsync(100);
         
-        var expected = new List<Coffee>
-        {
-            new Coffee{Id = 1, Name = "Cappuccino", Price = 500},
-            new Coffee{Id = 2, Name = "Latte", Price = 400},
-            new Coffee{Id = 3, Name = "Espresso", Price = 300}
-        };
-
-        for (int i = 0; i < expected.Count; ++i)
-        {
-            Assert.AreEqual(expected[i].Name, coffees.ToList()[i].Name);
-        }
+        Assert.AreEqual(100, banknote.Nominal);
+        Assert.AreEqual(2, banknote.Id);
     }
 
     [Test]
-    public async Task TestGetById()
+    public async Task GetBanknotesByMachineAsync()
     {
-        using var context = CreateContext();
-        var repository = new CoffeeRepository(context);
-        
-        var coffee = await repository.GetByIdAsync(1);
-        
-        Assert.AreEqual("Cappuccino", coffee.Name);
-    }
-
-    [Test]
-    public async Task TestAdd()
-    {
-        using var context = CreateContext();
-        var repository = new CoffeeRepository(context);
-        
-        var coffee = await repository.AddAsync(new Coffee{Id = 4, Name = "Americano", Price = 300});
-        
-        Assert.AreEqual("Americano", coffee.Name);
+        var data = new Data();
+        using var context = data.CreateContext();
+        var repository = new BanknoteRepository(context);
     }
 }
